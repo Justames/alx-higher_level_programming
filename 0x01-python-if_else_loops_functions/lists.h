468551890f7e9599d6e7f1b8a1562bebadc07460
root <root@DESKTOP-KFLC4OT>
2024-02-04 15:00:36 +0300

    Mand

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -6,5 +6,8 @@ def uppercaase(str):
 def uppercase(str):
 def print_last_digit(number):
 def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
 
 #endif
