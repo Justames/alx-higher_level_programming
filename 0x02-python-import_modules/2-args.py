a577a4da00b6c2cd393a100694565455fc5ef19c
Justames <jameswachirah39@gmail.com>
2024-03-30 09:58:50 +0300

    naba

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-import sys
-def main():
-     
-    num_args = len()
+
+if __name__ == "__main__"
+    """Print number of and arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usrbin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int9sys.argv[i + 1])
+        print("{}".format(totsl))
+
