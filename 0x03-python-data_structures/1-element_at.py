64387657411c018f4402ef37b984e5c56f31c782
Justames <jameswachirah39@gmail.com>
2024-05-11 09:38:12 +0300

    ok

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#usr/bin/python3
+"function that retrieves an element"
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return none
+    else:
+        return my_list[idx]
