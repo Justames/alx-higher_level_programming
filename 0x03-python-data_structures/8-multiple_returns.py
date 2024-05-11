cf89ce8c11fe7487ac29152377c691c8ac5dfde1
Justames <jameswachirah39@gmail.com>
2024-05-11 19:46:33 +0300

    hang on

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"tuple with length of dtring and first character"
+
+def multiple_returns(sentence):
+    if not sentence:
+        return none
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        tuple_1 = length, first
+        return tuple_1
