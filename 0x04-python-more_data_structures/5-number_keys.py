bb04cab405692b8ba2b27c4eae574f9eef2b76f2
Justames <jameswachirah39@gmail.com>
2024-05-27 15:36:28 +0300

    rihanna

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"no of keys in a dict"
+
+def number_keys(a_dictionary):
+    num = len(a_dictionary)
+    return num
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"sorted by keys"
+
+def print_sorted_dictionary(a_dictionary):
+    sort = sorted(a_dictionary)
+    return sort
